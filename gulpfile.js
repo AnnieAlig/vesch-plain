4c7ce2541f5592cc9965e6dafdb337e84453e8bd
Annie Alig <coolahhy@gmail.com>
2018-12-12 22:58:45 +0200

    to css4

--- gulpfile.js
+++ gulpfile.js
@@ -10,24 +10,28 @@ var postcss = require('gulp-postcss');
 
 var baseDir = '';
 
-gulp.task('sass', function() {
-    return gulp.src(baseDir + 'css/*.sass')
-        .pipe(sass().on( 'error', notify.onError(
-            {
-              message: "<%= error.message %>",
-              title  : "Sass Error!"
-            } ) )
-        )
+// gulp.task('sass', function() {
+//     return gulp.src(baseDir + 'css/*.sass')
+//         .pipe(sass().on( 'error', notify.onError(
+//             {
+//               message: "<%= error.message %>",
+//               title  : "Sass Error!"
+//             } ) )
+//         )
+//         .pipe(autoprefixer({
+//             browsers: ['last 2 versions'],
+//             cascade: false
+//         }))
+//         .pipe(gulp.dest(baseDir + 'dist/css/'))
+//         // .pipe( notify( 'SASS - хорошая работа!' ) );
+// })
+
+gulp.task('css', function () {
+    return gulp.src(baseDir + '/css/main.css')
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest(baseDir + 'dist/css/'))
-        // .pipe( notify( 'SASS - хорошая работа!' ) );
-})
-
-gulp.task('css', ['sass'], function () {
-    return gulp.src(baseDir + 'dist/css/style.css')
         .pipe(concatCss('stylemin.css'))
         // .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest(baseDir + 'dist/css/'))
